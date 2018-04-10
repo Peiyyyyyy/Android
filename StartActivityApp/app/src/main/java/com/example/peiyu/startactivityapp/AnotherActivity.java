74846744a197301d5683027ef4dde0e5f216c966
Peiyyyyyy <pyyyyy123@126.com>
2018-04-10 13:36:57 +0800

    Activity之间跳转

---
+++ StartActivityApp/app/src/main/java/com/example/peiyu/startactivityapp/AnotherActivity.java
@@ -0,0 +1,27 @@
+package com.example.peiyu.startactivityapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class AnotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+
+        Button button = (Button) findViewById(R.id.buttonReturn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AnotherActivity.this,
+                        MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
---
+++ StartActivityApp/app/src/main/java/com/example/peiyu/startactivityapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.peiyu.startactivityapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button bt = (Button) findViewById(R.id.buttonStartAnother);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,
+                        AnotherActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
